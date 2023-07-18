588563aebbb2ca24844a86e1deff49a2b5a021a1
xcomi <xaviercomi@gmail.com>
2023-07-18 07:58:57 +0200

    botones esconden ventana nosotros & habilitado descarga cv

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@ const ventana = document.getElementById('ventana-popup');
 const botonUnete = document.getElementById('boton-unete');
 const sectionWords = document.querySelector('.words');
 const wordsSpan = document.getElementById('span-words');
+const resource = document.getElementById('resources');
+const portafolio = document.getElementById('portafolio');
+const download = document.getElementById('download');
+const downloadCV = document.getElementById('download-cv');
 
 function muestraVentana() {
   ventana.classList.toggle('hidden');
@@ -13,6 +17,23 @@ function empiezaAnimacion() {
   wordsSpan.style.animationPlayState = "running";
 }
 
+function cierraVentana() {
+
+  if (ventana.classList.value = 'popup') {
+    ventana.classList.toggle('hidden');
+  }
+  console.log(ventana.classList);
+}
+
+
 nosotros.addEventListener('click', muestraVentana);
 
 botonUnete.addEventListener('mouseover', empiezaAnimacion);
+
+resource.addEventListener('mouseover', cierraVentana);
+
+portafolio.addEventListener('mouseover', cierraVentana);
+
+download.addEventListener('mouseover', cierraVentana);
+
+download.addEventListener('onclick', enterPassword);
