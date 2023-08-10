64ea2a86978695b6cc25b2cb07b1166cb24bb6d3
xcomi <xaviercomi@gmail.com>
2023-08-10 08:29:13 +0200

    React get from the public folder

--- work-team/src/index.js
+++ work-team/src/index.js
@@ -8,7 +8,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('MonstersRodolex')
 );
 
 // If you want to start measuring performance in your app, pass a function
